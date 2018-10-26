17de9156394434ef2a251029edc003971e65ee25
lorenaquint <lorenaquint@gmail.com>
2018-10-26 13:14:10 -0500

    agregando nuevas alertas
    Signed-off-by: lorenaquint <lorenaquint@gmail.com>

--- MOD1/js/alertas.js
+++ MOD1/js/alertas.js
@@ -7,6 +7,9 @@ alert("Bienvenido a Javascript");
 var conf = confirm("Desea salir de la aplicación");
 console.log("conf", conf);
 
+var conf2 = confirm("Bienvenido a Github");
+console.log("conf2",conf2);
+
 
 /* Prompt */
 var edad = prompt("Escribe tu edad", 0);
